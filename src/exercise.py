20af08031e8be6483397da319c75d8eb0c44808d
MrSullivan <>
2021-04-20 20:48:57 +0000

    variable at statt of loop, while true, number variable with integer input, if statement with equal to comparable, ending loop with break, if statement with not equal to statement, comand adding one to variable, print comand of variable.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    totalNumbers = 0
+
+    while True:
+      number = int(input("Give a number:"))
+      if (number == 0):
+        break
+    
+      if (number != 0):
+        totalNumbers = totalNumbers + 1
+
+    print("Number of numbers: " + str(totalNumbers))
 
 if __name__ == '__main__':
     main()
